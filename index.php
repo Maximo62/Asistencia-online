159d28b3ee4f8dba8fb68c81c69ba32e4cd2f7ec
Maximo <maximo.jones@unc.edu.ar>
2024-08-22 17:50:40 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'top.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de registro de asistencia</title>
+    <?php include 'top.php'; ?>
+</head>
+<body>
+    <h1>Sistema de registro de asistencia</h1>
+    <form>
+        <!-- Formulario de ejemplo -->
+    </form>
+    
+</body>
+</html>
---
+++ top.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>SIG - Sistema de Información para la Gestión</title>
+    
+    <!-- Incluir Bootstrap CSS desde local -->
+    <link rel="stylesheet" href="/libs/css/bootstrap.min.css">
+
+    <!-- Incluir jQuery, Popper.js, y Bootstrap JS desde local -->
+    <script src="/libs/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="/libs/js/popper.min.js"></script>
+    <script src="/libs/js/bootstrap.min.js"></script>
+ 
+</head>
